aeabeb08238afccf66a31fc371e1c02852ab2055
Nataly Makarova <natmaklis@gmail.com>
2020-05-05 17:24:10 +0300

    Updates

---
+++ precache-manifest.0ca389d2797db67868b43c52ea58d3b8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6d0506fc8870b659e9353b58862a4dd",
+    "url": "/WeatherAPI__react/index.html"
+  },
+  {
+    "revision": "75dd2774ea6d7d268b4c",
+    "url": "/WeatherAPI__react/static/css/main.07bc0778.chunk.css"
+  },
+  {
+    "revision": "e1b21e3ae8a164e3490e",
+    "url": "/WeatherAPI__react/static/js/2.e332319d.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/WeatherAPI__react/static/js/2.e332319d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "75dd2774ea6d7d268b4c",
+    "url": "/WeatherAPI__react/static/js/main.ea18f34c.chunk.js"
+  },
+  {
+    "revision": "888010bbe9ca093f7b31",
+    "url": "/WeatherAPI__react/static/js/runtime-main.8037b1be.js"
+  },
+  {
+    "revision": "246c1201edbfbe27c6efc421dbc7eec5",
+    "url": "/WeatherAPI__react/static/media/2850815.246c1201.jpg"
+  }
+]);
